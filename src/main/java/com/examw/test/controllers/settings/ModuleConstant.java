55cfef0edc4e6815fb964a2f6a1259dcff7221d5
vigo2013 <fw121fw4@163.com>
2014-08-06 16:34:36 +0800

    增加考试分类菜单常量代码

--- src/main/java/com/examw/test/controllers/settings/ModuleConstant.java
+++ src/main/java/com/examw/test/controllers/settings/ModuleConstant.java
@@ -9,4 +9,16 @@ public final class ModuleConstant {
 	 * 地区设置ID。
 	 */
 	public static final String SETTINGS_AREA = "ETS0000102";
+	/**
+	 * 考试分类ID。
+	 */
+	public static final String SETTINGS_CATEGORY = "ETS0000202";
+	/**
+	 * 考试设置ID。
+	 */
+	public static final String SETTINGS_EXAM = "ETS0000302";
+	/**
+	 * 科目设置ID。
+	 */
+	public static final String SETTINGS_SUBJECT = "ETS0000402";
 }
