da7a36f2af941b0c7280df60f633799466bf788e
jeasonyoung <jeason1914@qq.com>
2015-02-27 11:45:43 +0800

    修正注册码生成时激活保存bug

--- src/main/java/com/examw/test/service/products/impl/RegistrationServiceImpl.java
+++ src/main/java/com/examw/test/service/products/impl/RegistrationServiceImpl.java
@@ -207,7 +207,9 @@ public class RegistrationServiceImpl extends BaseDataServiceImpl<Registration,Re
 			if(info.getCreateTime() == null) info.setCreateTime(new Date());
 		}
 		info.setLastTime(new Date());
-		if(info.getStatus() == null) info.setStatus(RegistrationStatus.NONE.getValue());
+		if(info.getStatus() == null){
+			info.setStatus(RegistrationStatus.NONE.getValue());
+		}
 		try {
 			if(!this.verificationFormat(info.getCode())){
 				throw new Exception(String.format("非法注册码:%s", info.getCode()));
@@ -217,7 +219,7 @@ public class RegistrationServiceImpl extends BaseDataServiceImpl<Registration,Re
 			throw new RuntimeException(e.getMessage());
 		}
 		//创建注册码激活
-		if(info.getLimits() > 0 && info.getStatus() == RegistrationStatus.ACTIVE.getValue() && (data.getStatus() != RegistrationStatus.ACTIVE.getValue() || data.getStartTime() == null)){
+		if(info.getLimits() > 0 && info.getStatus() == RegistrationStatus.ACTIVE.getValue()){
 			data.setStartTime(new Date());//激活时间
 			Calendar calendar = Calendar.getInstance();
 			calendar.setTime(data.getStartTime());
